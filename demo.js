3afd7bd6b7ef009e115d7184fb296956a197d918
Mahmoud-Barry <mahmoudbarrysn@gmail.com>
2021-07-01 14:26:12 +0200

    objects

--- demo.js
+++ demo.js
@@ -1,6 +1,12 @@
 'use strict'; 
 (function() {
 
-  display('Hello World');
+  let person = {
+    fitstName: "Mahmoud",
+    lastName: "Barry",
+    age: 20,
+    isAdult: function(){ return person.age >=18}
+  };
+  display(person.isAdult());
 
 })();
